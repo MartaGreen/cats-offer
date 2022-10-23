88295f7733088fb418a9a885f01d0945face5fac
mariik <m.portnykh@fxdev.sk>
2022-10-23 13:12:15 +0200

    fix: change browser router to hash router

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styles from "./App.style";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "../../redux";
 
